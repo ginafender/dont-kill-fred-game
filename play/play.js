969baf7796114147a9795cc55ae307461260e04a
ginafender <123966430+ginafender@users.noreply.github.com>
2024-04-19 10:53:07 -0400

    added canvas and updated play files

---
+++ play/play.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+});
